2e1b2d80d33b05b7c2eadeff0d3c8e0bb34e34f1
Anatoly Laskaris <github_me@nahsi.dev>
2023-04-13 15:26:53 +0300

    Add action draft

---
+++ index.js
@@ -0,0 +1,87 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const tc = require("@actions/tool-cache");
+const fs = require('fs');
+const axios = require("axios");
+const semver = require("semver");
+
+const osArchMapping = {
+  "macos": {
+    "x64": "darwin_amd64",
+    "arm64": "darwin_arm64",
+  },
+  "linux": {
+    "x64": "linux_amd64",
+    "arm64": "linux_arm64",
+  },
+};
+
+function getArchitecture(runnerOS, arch) {
+  if (
+    osArchMapping.hasOwnProperty(runnerOS) &&
+    osArchMapping[runnerOS].hasOwnProperty(arch)
+  ) {
+    return osArchMapping[runnerOS][arch];
+  }
+
+  core.setFailed(`${runnerOS} with architecture ${arch} is not supported yet`);
+  return null;
+}
+
+async function run() {
+  try {
+    let version = core.getInput("version");
+    const toolName = core.getInput("name");
+    const runnerOS = process.env.RUNNER_OS.toLowerCase();
+    const runnerArch = process.arch;
+
+    const architecture = getArchitecture(runnerOS, runnerArch);
+    if (!architecture) {
+      return;
+    }
+
+    if (version.toLowerCase() === "latest") {
+      core.info("Fetching latest version");
+      const response = await axios.get(
+        `https://releases.hashicorp.com/${toolName}/index.json`,
+      );
+      const nonEnterpriseVersions = Object.keys(response.data.versions)
+        .filter((version) =>
+          !version.includes("+ent") && semver.valid(version)
+        );
+
+      const latestVersion = nonEnterpriseVersions.sort(semver.rcompare)[0];
+
+      version = latestVersion;
+    } else {
+      version = version.replace(/^v/, "");
+    }
+
+    const zipFilename = `${toolName}_${version}_${architecture}.zip`;
+    const downloadUrl =
+      `https://releases.hashicorp.com/${toolName}/${version}/${zipFilename}`;
+
+    core.info(`Downloading ${toolName} from ${downloadUrl}`);
+    const downloadedPath = await tc.downloadTool(downloadUrl);
+
+    core.info(`Extracting ${toolName} binary`);
+    const extractedPath = await tc.extractZip(downloadedPath);
+
+    const binaryPath = `${extractedPath}/${toolName}`;
+    core.info(`Adding ${toolName} to PATH`);
+    await tc.cacheFile(binaryPath, toolName, toolName, version);
+
+    core.info("Making the tool executable");
+    fs.chmodSync(`${extractedPath}/${toolName}`, '755');
+
+    core.info("Checking installed version");
+    await exec.exec(`${toolName}`, ["--version"]);
+
+    core.info("Cleaning up residual files");
+    await exec.exec("rm", ["-rf", downloadedPath]);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
